f056c42d1a3ce6e9012d0c5c2b84845172369375
Jeffrey-1098684 <jeffrey.jubal@gmail.com>
2025-04-13 15:32:48 +0200

    Initial commit ofzo iets

---
+++ script.js
@@ -0,0 +1,17 @@
+const countdown = () => {
+    const eventDate = new Date('July 11, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+  
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById('days').innerText = days.toString().padStart(2, '0');
+    document.getElementById('hours').innerText = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').innerText = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').innerText = seconds.toString().padStart(2, '0');
+  }
+  
+  setInterval(countdown, 1000);
